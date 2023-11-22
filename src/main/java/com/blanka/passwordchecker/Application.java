ffa7d29238065297284b4b64951437cbf41c81d4
LBlanka99 <losonci.blanka@mensa.hu>
2023-11-22 15:43:11 +0100

    create project structure

--- src/main/java/com/blanka/passwordchecker/Application.java
+++ src/main/java/com/blanka/passwordchecker/Application.java
@@ -1,4 +1,7 @@
-public class Main {
+package com.blanka.passwordchecker;
+
+public class Application {
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
