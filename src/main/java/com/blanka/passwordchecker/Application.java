7c976d4fdc08acd9fc8408ec3c09adf44cf5811f
LBlanka99 <losonci.blanka@mensa.hu>
2023-11-22 22:09:59 +0100

    instantiate needed classes in the Application

--- src/main/java/com/blanka/passwordchecker/Application.java
+++ src/main/java/com/blanka/passwordchecker/Application.java
@@ -1,8 +1,32 @@
 package com.blanka.passwordchecker;
 
+
+import com.blanka.passwordchecker.logic.CorrectPasswordCalculator;
+import com.blanka.passwordchecker.logic.rules.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    public static final String FILE_PATH = "src/main/resources/input.txt";
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        EndingPunctuationValidator endingPunctuationValidator = new EndingPunctuationValidator();
+        LengthValidator lengthValidator = new LengthValidator();
+        LowerCaseLetterValidator lowerCaseLetterValidator = new LowerCaseLetterValidator();
+        UniqueWordsValidator uniqueWordsValidator = new UniqueWordsValidator();
+        UnnecessarySpacesValidator unnecessarySpacesValidator = new UnnecessarySpacesValidator();
+        List<PasswordValidator> rules = new ArrayList<>(
+                List.of(endingPunctuationValidator,
+                        lengthValidator,
+                        lowerCaseLetterValidator,
+                        uniqueWordsValidator,
+                        unnecessarySpacesValidator)
+        );
+
+        CorrectPasswordCalculator calculator = new CorrectPasswordCalculator(FILE_PATH, rules);
+        int result = calculator.run();
+        System.out.println(result);
     }
 }
