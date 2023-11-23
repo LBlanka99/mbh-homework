038903edabbe38278f9ee5ed8b3e55992c957222
LBlanka99 <losonci.blanka@mensa.hu>
2023-11-23 12:06:16 +0100

    remove unnecessary null check from LengthValidator

--- src/main/java/com/blanka/passwordchecker/logic/rules/LengthValidator.java
+++ src/main/java/com/blanka/passwordchecker/logic/rules/LengthValidator.java
@@ -4,9 +4,6 @@ package com.blanka.passwordchecker.logic.rules;
 public class LengthValidator implements PasswordValidator {
     @Override
     public boolean isValid(String password) {
-        if (password == null) {
-            return false;
-        }
         String[] words = password.split(" ");
         return words.length >= 2;
     }
--- src/test/java/com/blanka/passwordchecker/logic/rules/LengthValidatorTest.java
+++ src/test/java/com/blanka/passwordchecker/logic/rules/LengthValidatorTest.java
@@ -1,23 +1,12 @@
 package com.blanka.passwordchecker.logic.rules;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class LengthValidatorTest {
     LengthValidator validator = new LengthValidator();
-
-    @ParameterizedTest
-    @NullAndEmptySource
-    @ValueSource(strings = {"  ", "\t", "\n"})
-    void testForAllTypesOfBlankStrings(String password) {
-        boolean result = validator.isValid(password);
-        assertFalse(result);
-    }
 
     @Test
     void testWithOneWord() {
