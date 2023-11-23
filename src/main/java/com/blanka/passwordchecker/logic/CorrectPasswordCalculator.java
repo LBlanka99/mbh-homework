3e51f9568b53a80447c831017bb3ccf672c96936
LBlanka99 <losonci.blanka@mensa.hu>
2023-11-23 12:16:57 +0100

    check if the input is null or blank before pass it to validators

--- src/main/java/com/blanka/passwordchecker/logic/CorrectPasswordCalculator.java
+++ src/main/java/com/blanka/passwordchecker/logic/CorrectPasswordCalculator.java
@@ -1,6 +1,7 @@
 package com.blanka.passwordchecker.logic;
 
 import com.blanka.passwordchecker.data.FileReader;
+import com.blanka.passwordchecker.logic.inputchecker.InputChecker;
 import com.blanka.passwordchecker.logic.rules.PasswordValidator;
 import com.blanka.passwordchecker.ui.Screen;
 
@@ -10,11 +11,13 @@ public class CorrectPasswordCalculator {
     private final String FILE_PATH;
     private final List<PasswordValidator> rules;
     private final Screen screen;
+    private final InputChecker inputChecker;
 
     public CorrectPasswordCalculator(String FILE_PATH, List<PasswordValidator> rules) {
         this.FILE_PATH = FILE_PATH;
         this.rules = rules;
         screen = new Screen();
+        inputChecker = new InputChecker();
     }
 
     public void run() {
@@ -29,6 +32,10 @@ public class CorrectPasswordCalculator {
 
         int counter = 0;
         for (String password : sentences) {
+            if (inputChecker.isInputBlankOrNull(password)) {
+                continue;
+            }
+
             boolean isCorrect = true;
             for (PasswordValidator rule : rules) {
                 if (!rule.isValid(password)) {
