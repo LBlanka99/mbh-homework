790ba0ba1ebd2eb57bd672d9cf72a1f823d2e6cf
LBlanka99 <losonci.blanka@mensa.hu>
2023-11-23 10:33:30 +0100

    test LengthValidator for all blank input

--- src/test/java/com/blanka/passwordchecker/logic/rules/LengthValidatorTest.java
+++ src/test/java/com/blanka/passwordchecker/logic/rules/LengthValidatorTest.java
@@ -1,6 +1,9 @@
 package com.blanka.passwordchecker.logic.rules;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -8,9 +11,10 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class LengthValidatorTest {
     LengthValidator validator = new LengthValidator();
 
-    @Test
-    void testEmptyString() {
-        String password = "";
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {"  ", "\t", "\n"})
+    void testForAllTypesOfBlankStrings(String password) {
         boolean result = validator.isValid(password);
         assertFalse(result);
     }
